e77fec23340047dfd228a1b05e90d68029095cc5
jknudtson2 <46786261+jknudtson2@users.noreply.github.com>
2019-01-17 08:34:41 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jenna Knudtson
+ * Date: 2019/01/15
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jenna Lynne Knudtson\n");
+
+  return 0;
+}
